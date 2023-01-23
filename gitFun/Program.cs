38e487f0ab19a8ef622577c05530697d7687a0aa
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-01-23 16:01:28 -0700

    I did a little something

--- gitFun/Program.cs
+++ gitFun/Program.cs
@@ -6,7 +6,7 @@ namespace gitFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello I did something wood World!");
         }
     }
 }
