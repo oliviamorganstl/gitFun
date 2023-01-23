f679067768f35adcb291b82b75d6f9e3e477fabf
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-01-23 15:58:06 -0700

    Add project files.

---
+++ gitFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
